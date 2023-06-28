90483ac91f8e8b431a89b00ff392dfa38a5731c5
Codedlio <alexis.leonel_@hotmail.com>
2023-06-28 16:59:13 -0600

    firs commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <div className='bg-red-400'>
+
+      <title>Alex Portfolio</title>
+
+      <h1 className="">VAMONOS RECIO</h1>
+    </div>
+  )
+}
